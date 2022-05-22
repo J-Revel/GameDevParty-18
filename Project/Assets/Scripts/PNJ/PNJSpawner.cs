cf76a43a9114d622df8d2c4fee7bba5e511476bd
J-Revel <j.revel@protonmail.com>
2022-05-22 14:08:10 +0200

    Show character profile

--- Project/Assets/Scripts/PNJ/PNJSpawner.cs
+++ Project/Assets/Scripts/PNJ/PNJSpawner.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
 
 public class PNJSpawner : MonoBehaviour
 {
     public float leftWingProbability = 0.4f;
+    public PNJConfig config;
     public PNJProfile pnjPrefab;
     public SpriteAnimList[] animationSets;
+    public SpriteAnimList[] animationSetsMale;
+    public SpriteAnimList[] animationSetsFemale;
     public int minInterestCount = 1;
     public int maxInterestCount = 5;
     public BoxCollider spawnBox;
@@ -52,8 +56,37 @@ public class PNJSpawner : MonoBehaviour
             availableThemes.RemoveAt(index);
         }
         pnj.leftWing = Random.Range(0, 1.0f) < leftWingProbability;
-        pnj.GetComponentInChildren<AnimatedSprite>().animList = animationSets[Random.Range(0, animationSets.Length)];
+        StringBuilder idCardContent = new StringBuilder();
+        if(Random.Range(0, 1.0f) > 0.5f)
+        {
+            pnj.genre = Genre.Male;
+            pnj.GetComponentInChildren<AnimatedSprite>().animList = animationSetsMale[Random.Range(0, animationSetsMale.Length)];
+            idCardContent.Append(config.menFirstNames[Random.Range(0, config.menFirstNames.Length)]);
+        }
+        else
+        {
+            pnj.genre = Genre.Female;
+            pnj.GetComponentInChildren<AnimatedSprite>().animList = animationSetsFemale[Random.Range(0, animationSetsFemale.Length)];
+            idCardContent.Append(config.womenFirstNames[Random.Range(0, config.womenFirstNames.Length)]);
+        }
+
+        idCardContent.Append(" ").Append(config.secondNames[Random.Range(0, config.secondNames.Length)]).Append("\n");
+        List<IdCardCategory> presentCategories = new List<IdCardCategory>();
+        for(int i=0; i<3; i++)
+        {
+            IdCardInfoConfig info = config.info[Random.Range(0, config.info.Length)];
+            if(presentCategories.Contains(info.category))
+                continue;
+            else presentCategories.Add(info.category);
+            if(info.isInvalid)
+                pnj.canVote = false;
+            idCardContent.Append(info.text).Append("\n");
+        }
+        pnj.idCard = idCardContent.ToString();
+        
+        
         pnj.GetComponent<CharacterMovement>().movementInput = transform.forward;
+
         
         if(targetBox != null)
             pnj.GetComponent<RandomPNJMovement>().initialTarget = new Vector3(Random.Range(targetBox.bounds.min.x, targetBox.bounds.max.x), 0, Random.Range(targetBox.bounds.min.z, targetBox.bounds.max.z));
