4a7f8a23814ea65b29ba9d5f7a33ed9f2c24323a
Clement Ribeyre-Soret <clement.ribeyres@gmail.com>
2022-06-01 16:40:30 +0200

    Update Pigeon behavior

---
+++ Project/Assets/Scripts/PNJ/TriggerPigeonFly.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPigeonFly : MonoBehaviour
+{
+    [SerializeField] private Animator PigeonFlying;
+
+    [SerializeField] private string PigeonFly = "PigeonFly";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PigeonFlying.SetInteger("PigeonFly_index", Random.Range(0, 2));
+            PigeonFlying.SetTrigger("CharacterEnterCollider");
+        }
+    }
+}
